b40178f32ffd5c4a4a2843fba5748ef2503495a4
ahmed45316 <ahmattaallah@gmail.com>
2021-01-25 23:52:28 +0200

    Add project files.

---
+++ ChannelsTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ChannelsTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var obj = new ChannelsClass();
+            await obj.ChannelSTest();
+            Console.ReadLine();
+        }
+    }
+    public class ChannelsClass
+    {
+        public async Task ChannelSTest()
+        {
+            var ch = Channel.CreateUnbounded<string>();
+
+            var consumer = Task.Run(async () =>
+            {
+                while (await ch.Reader.WaitToReadAsync())
+                    Console.WriteLine(await ch.Reader.ReadAsync());
+            });
+            var producer = Task.Run(async () =>
+            {
+                var address = new Address { NameAr = "ahmed", NameEn = "Mohamed" };
+                var output = JsonConvert.SerializeObject(address);
+                await ch.Writer.WriteAsync(output);
+                ch.Writer.Complete();
+            });
+
+            await Task.WhenAll(producer, consumer);
+        }
+    }
+    public class Address
+    {
+        public string NameEn { get; set; }
+        public string NameAr { get; set; }
+    }
+}
